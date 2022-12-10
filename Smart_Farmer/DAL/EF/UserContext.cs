3f0112a78b333f97d31ed1e12a8fb528803955e1
Riderr470 <riderr40@gmail.com>
2022-12-10 15:51:47 +0600

    User table test

---
+++ Smart_Farmer/DAL/EF/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using DAL.EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF
+{
+    public class UserContext: DbContext
+    {
+        public  DbSet<User> Users { get; set; }
+    }
+}
